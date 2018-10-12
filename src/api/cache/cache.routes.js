8fc606dd06fa3083f2009144b775c71058dc4772
Hassan Oyeboade <oyeboadehassan@gmail.com>
2018-10-13 00:04:41 +0100

    Update cache api routes

--- src/api/cache/cache.routes.js
+++ src/api/cache/cache.routes.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cacheController from "./cache.controller";
 const router = express.Router([]);
 
-router.get('/cache/keys/', cacheController.getAll);
-router.delete('/cache/keys/', cacheController.deleteAll);
+router.get('/cache/', cacheController.getAll);
+router.delete('/cache/', cacheController.deleteAll);
 
 router.get('/cache/:key/', cacheController.getOne);
 router.post('/cache/:key/', cacheController.create);
